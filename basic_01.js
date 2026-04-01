626a84d8f4dcd6835f936abcaf4fd0157074c4be
Ashish Kumar <Ashishkr403@gmail.com>
2026-04-01 22:28:00 +0530

    Java script basis

---
+++ basic_01.js
@@ -0,0 +1,12 @@
+let a=5, b=1
+let c = a+b
+c = "Ashish"
+let required = true
+console.log("Ram Ram" , c)
+console.log(c)
+
+
+console.log(typeof(c))
+console.log(required)
+console.log(!required)
+console.log(typeof(required))
